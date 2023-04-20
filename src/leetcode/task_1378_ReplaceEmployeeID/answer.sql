e34fd9b46df33926e2e34963b81b2a64b2922d83
RealCryZed <bujhmgtnhjd@bk.ru>
2023-04-20 12:30:28 +0600

    Added new tasks

---
+++ src/leetcode/task_1378_ReplaceEmployeeID/answer.sql
@@ -0,0 +1,2 @@
+select EmployeeUNI.unique_id, Employees.name from Employees
+LEFT JOIN EmployeeUNI ON Employees.id = EmployeeUNI.id;
---
+++ src/leetcode/task_9_PalindromeNumber/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.task_9_PalindromeNumber;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+
+        if (str.length() == 1) return true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length()-1-i)) return false;
+        }
+
+        return true;
+    }
+}
